883071af489ad8e076a8eef81668c64b2c2de855
sergio.sanchez <sergio.sanchez@liferay.com>
2019-11-05 21:27:57 +0100

    First-commit: Samples for accessibility plugin setup for CKEditor in Liferay DXP 7.1

---
+++ modules/gcanta-editor-accesibilidad/bin/main/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp"%>
+
+<p>
+	<b><liferay-ui:message key="gcantaeditoraccesibilidad.caption" /></b>
+</p>
---
+++ modules/gcanta-editor-accesibilidad/src/main/java/gcanta/editor/accesibilidad/constants/GcantaEditorAccesibilidadPortletKeys.java
@@ -0,0 +1,11 @@
+package gcanta.editor.accesibilidad.constants;
+
+/**
+ * @author sergios
+ */
+public class GcantaEditorAccesibilidadPortletKeys {
+
+	public static final String GCANTAEDITORACCESIBILIDAD =
+		"gcanta_editor_accesibilidad_GcantaEditorAccesibilidadPortlet";
+
+}
