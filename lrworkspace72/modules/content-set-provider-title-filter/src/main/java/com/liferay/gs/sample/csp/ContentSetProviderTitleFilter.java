f61caeab9e5ce09b0fd0ec6cc90bcc9280fe4acc
sergio.sanchez <sergio.sanchez@liferay.com>
2020-05-14 23:11:23 +0200

    Content Set Provider methods

--- lrworkspace72/modules/content-set-provider-title-filter/src/main/java/com/liferay/gs/sample/csp/ContentSetProviderTitleFilter.java
+++ lrworkspace72/modules/content-set-provider-title-filter/src/main/java/com/liferay/gs/sample/csp/ContentSetProviderTitleFilter.java
@@ -9,10 +9,15 @@ import com.liferay.info.provider.InfoListProviderContext;
 import com.liferay.info.sort.Sort;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -31,8 +36,37 @@ public class ContentSetProviderTitleFilter implements InfoListProvider<AssetEntr
 
 	@Override
 	public List<AssetEntry> getInfoList(InfoListProviderContext infoListProviderContext) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Company company = infoListProviderContext.getCompany();
+		Optional<Group> groupOptional = infoListProviderContext.getGroupOptional();
+		
+		Group group = groupOptional.get();
+		
+		long companyId = company.getCompanyId();
+		long[] groupIds = null;
+		long userId = 0;
+		//String className = "com.liferay.journal.model.JournalArticle";
+		String className = null;
+		long classTypeId = 0;
+		String userName = null;
+		String title = _titleInfoListConfiguration.assetTitle();
+		String description = null;
+		String assetCategoryIds = null;
+		String assetTagNames = null;
+		int[] statuses = {WorkflowConstants.STATUS_APPROVED};
+		boolean showNonindexable = true;
+		boolean andSearch = false;	
+		int start = -1;
+		int end = -1;
+		
+	
+		Hits hits = _assetEntryLocalService.search(companyId, groupIds, userId, className, classTypeId, userName,
+			title, description, assetCategoryIds, assetTagNames, showNonindexable, statuses, andSearch, start, end);
+			
+		
+		List<AssetEntry> assetEntries = _assetHelper.getAssetEntries(hits);
+		
+		return assetEntries;
 	}
 
 	@Override
