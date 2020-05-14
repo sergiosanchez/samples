2ce1dde833e08ab5c819659bd4eccc996c5dc064
sergio.sanchez <sergio.sanchez@liferay.com>
2020-05-14 23:05:54 +0200

    Content Set Provider class structure

--- lrworkspace72/modules/content-set-provider-title-filter/src/main/java/com/liferay/gs/sample/csp/ContentSetProviderTitleFilter.java
+++ lrworkspace72/modules/content-set-provider-title-filter/src/main/java/com/liferay/gs/sample/csp/ContentSetProviderTitleFilter.java
@@ -1,6 +1,13 @@
 package com.liferay.gs.sample.csp;
 
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.info.pagination.Pagination;
 import com.liferay.info.provider.InfoListProvider;
+import com.liferay.info.provider.InfoListProviderContext;
+import com.liferay.info.sort.Sort;
+
+import java.util.List;
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -8,13 +15,36 @@ import org.osgi.service.component.annotations.Component;
  * @author sergios
  */
 @Component(
+	configurationPid = "com.liferay.gs.TitleInfoListConfig",
 	immediate = true,
-	property = {
-		// TODO enter required service properties
-	},
 	service = InfoListProvider.class
 )
-public class ContentSetProviderTitleFilter implements InfoListProvider {
+public class ContentSetProviderTitleFilter implements InfoListProvider<AssetEntry> {
+
+	@Override
+	public List<AssetEntry> getInfoList(InfoListProviderContext infoListProviderContext) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AssetEntry> getInfoList(InfoListProviderContext infoListProviderContext, Pagination pagination,
+			Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getInfoListCount(InfoListProviderContext infoListProviderContext) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	// TODO enter required service methods
 
